c2e77e76ab8de83e6903255d5607353ce0cda68f
Hessam Mehr <hessammehr@gmail.com>
2016-07-15 16:00:15 -0700

    Fragment interface added.

--- core.ts
+++ core.ts
@@ -3,6 +3,11 @@ type node = {pos: number[]};
 type state = {nodes: Map<number,node>,
 			  bonds: Map<number,bond>};
 
+interface Fragment {
+	nodes: Map<number,node>,
+	bonds: Map<number,bond>
+}
+
 let state : state = 
 		{nodes: new Map( [[1, {pos: [0.0, 0.0]}],
 				 		  [2, {pos: [10.0, 10.0]}],
@@ -11,18 +16,18 @@ let state : state =
 						  [2, {nodes: new Set([1, 3])}]] )};
 
 
-function deleteNode(s:state, n:number) : void {
-	delete state.nodes[n];
+function deleteNode(f:Fragment, n:number) : void {
+	delete f.nodes[n];
 }
 
-function deleteBond(s:state, b:number) : void {
-	delete state.bonds[b];
+function deleteBond(f:Fragment, b:number) : void {
+	delete f.bonds[b];
 }
 
 // Returns an array of nodes connected to node #n
-function connectedNodes(s: state, n:number) : node[] {
+function connectedNodes(f:Fragment, n:number) : node[] {
 	let res = [];
-	for (let b of state.bonds.values()) {
+	for (let b of f.bonds.values()) {
 		if (b.nodes.has(n)) {
 			for (let i of b.nodes)
 				if (i !== n)
@@ -31,5 +36,3 @@ function connectedNodes(s: state, n:number) : node[] {
 	}
 	return res;
 }
-
-console.log("Hello");
