13465c0862398ffd2466c8ab76cba1207eb1401d
Hessam Mehr <hessammehr@gmail.com>
2016-07-16 16:16:07 -0700

    Moved from modules to flat bundling. No dependency testing.

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # molsketch-ts
-A reimplementation of molsketch in TypeScript.
+A reimplementation of molsketch-cljs in TypeScript.
 
 # Hacking on molsketch-ts
 First install TypeScript, `npm install -g typescript`, then
@@ -9,7 +9,15 @@ tsc -w
 python3 -m http.server
 ```
 
-and go to `localhost:8000`. Happy hacking!
+and go to `localhost:8000`.
 
+To run unit tests
+```bash
+cd test
+tsc -w
+python3 -m http.server   # if not already running in parent folder
+```
+
+Happy hacking!
 # License
 MIT
--- core.ts
+++ core.ts
@@ -1,36 +1,34 @@
-import { Point } from "./geometry"
-export * from "./geometry"
-export type bond = {nodes: Set<number>}
-export type node = {pos: Point}
-export type state = {nodes: Map<number,node>,
+type bond = {nodes: Set<number>}
+type node = {pos: Point}
+type state = {nodes: Map<number,node>,
 			  bonds: Map<number,bond>}
-export type template = {nodes: Map<number,node>,
+type template = {nodes: Map<number,node>,
 				 bonds: Map<number,bond>,
 				 // The root is the node or bond used
 				 // to connect the template to the rest
 				 // of the molecule.
 				 root: {node: number, bond: number}}
 
-export interface Fragment {
+interface Fragment {
 	nodes: Map<number,node>,
 	bonds: Map<number,bond>
 }
 
 // Removes node number n from fragment-like f. Returns f.
-export function deleteNode<F extends Fragment>(f:F, n:number) : F {
+function deleteNode<F extends Fragment>(f:F, n:number) : F {
 	delete f.nodes[n];
 	return f
 }
 
 // Removes bond number b from fragment-like f. Returns f.
-export function deleteBond<F extends Fragment>(f:F, b:number) : F {
+function deleteBond<F extends Fragment>(f:F, b:number) : F {
 	delete f.bonds[b];
 	return f
 }
 
 // Returns a set containing the numbers of nodes connected to
 // node number n in fragment-like f.
-export function connectedNodes(f:Fragment, n:number) : Set<number> {
+function connectedNodes(f:Fragment, n:number) : Set<number> {
 	let res = new Set<number>();
 	for (let b of f.bonds.values()) {
 		if (b.nodes.has(n)) {
@@ -44,7 +42,7 @@ export function connectedNodes(f:Fragment, n:number) : Set<number> {
 
 // Returns a set containing the numbers of bonds connecting
 // node number n to other nodes.
-export function connectingBonds(f:Fragment, n:number) : Set<number> {
+function connectingBonds(f:Fragment, n:number) : Set<number> {
 	let res = new Set<number>();
 	for (let [bn, b] of f.bonds.entries()) {
 		if (b.nodes.has(n))
