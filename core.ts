1ebd8a2f2987565d5a5e26683d4cbd1a31b56b59
Hessam Mehr <hessammehr@gmail.com>
2016-07-15 19:02:25 -0700

    Begin adding unit tests using jasmine.

--- core.ts
+++ core.ts
@@ -1,7 +1,9 @@
-type bond = {nodes: Set<number>};
-type node = {pos: number[]};
+type point = number[]
+type matrix = number[][]
+type bond = {nodes: Set<number>}
+type node = {pos: point}
 type state = {nodes: Map<number,node>,
-			  bonds: Map<number,bond>};
+			  bonds: Map<number,bond>}
 
 interface Fragment {
 	nodes: Map<number,node>,
@@ -13,26 +15,46 @@ let state : state =
 				 		  [2, {pos: [10.0, 10.0]}],
 						  [3, {pos: [0.0, 20.0]}]] ),
 		 bonds: new Map( [[1, {nodes: new Set([1, 2])}],
-						  [2, {nodes: new Set([1, 3])}]] )};
+						  [2, {nodes: new Set([1, 3])}]] )}
 
-
-function deleteNode(f:Fragment, n:number) : void {
+// Removes node number n from fragment-like f. Returns f.
+function deleteNode<F extends Fragment>(f:F, n:number) : F {
 	delete f.nodes[n];
+	return f
 }
 
-function deleteBond(f:Fragment, b:number) : void {
+// Removes bond number b from fragment-like f. Returns f.
+function deleteBond<F extends Fragment>(f:F, b:number) : F {
 	delete f.bonds[b];
+	return f
 }
 
-// Returns an array of nodes connected to node #n
-function connectedNodes(f:Fragment, n:number) : node[] {
-	let res = [];
+// Returns a set containing the numbers of nodes connected to
+// node number n in fragment-like f.
+function connectedNodes(f:Fragment, n:number) : Set<number> {
+	let res = new Set<number>();
 	for (let b of f.bonds.values()) {
 		if (b.nodes.has(n)) {
 			for (let i of b.nodes)
-				if (i !== n)
-					res.push(i);
+				res.add(i);
 		}
 	}
-	return res;
+	res.delete(n);
+	return res
+}
+
+// Returns a set containing the numbers of bonds connecting
+// node number n to other nodes.
+function connectingBonds(f:Fragment, n:number) : Set<number> {
+	let res = new Set<number>();
+	for (let [bn, b] of f.bonds.entries()) {
+		if (b.nodes.has(n))
+			res.add(bn);
+	}
+	return res
+}
+
+// Returns point p transformed by matrix m.
+function transformPoint(p:point, m:matrix) : point {
+	return [ p[0]*m[0][0]+p[1]*m[0][1], p[0]*m[1][0]+p[1]*m[1][1] ]
 }
