3ac7fb348824ea283d93f75269d9b935eacf9c7b
Hessam Mehr <hessammehr@gmail.com>
2016-11-21 01:34:33 -0800

    Updated tests.

--- test/tests.ts
+++ test/tests.ts
@@ -1,10 +1,14 @@
+let a: Atom = { pos: [1.0, 2.0] };
+
 // Setup
-let s : state =
-	{nodes: new Map( [[1, {pos: new Point(0.0,0.0)}],
-			 		  [2, {pos: new Point(10.0, 10.0)}],
-					  [3, {pos: new Point(0.0, 20.0)}]] ),
-	 bonds: new Map( [[1, {nodes: new Set([1, 2])}],
-					  [2, {nodes: new Set([1, 3])}]] )};
+let s: State = {
+	atoms: new Map(),
+	bonds: new Map([[1, { atoms: new Set([1, 2]) }],
+	[2, { atoms: new Set([1, 3]) }]])
+};
 
 console.log("Running tests...");
-console.log(connectedNodes(s, 2));
+console.log(max_atom(s));
+console.log(add_atom(s, a));
+console.log(max_atom(s));
+console.log(connectedAtoms(s, 2));
